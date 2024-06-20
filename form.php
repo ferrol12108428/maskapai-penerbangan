d37820c8c98b14a3673f5c3aaf510c3941aa6b0b
vajra20 <benediktusvajrasagara@smkwikrama.sch.id>
2024-06-21 05:32:24 +0700

    update penamaan

--- form.php
+++ form.php
@@ -2,74 +2,225 @@
 <html lang="id">
 
 <head>
-  <title>Rute Penerbangan</title>
-  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <!-- Title Browser Start-->
+    <title>Rute Penerbangan</title>
+  <!-- Title Browser End-->
+
+  <!-- Link Bootstrap Start -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <!-- Link Bootstrap End -->
 </head>
 
 <body>
-  <div class="container">
-    <h1 class="mt-5">Pendaftaran Rute Penerbangan</h1>
-    <form method="POST" action="action.php">
-      <div class="form-group">
-        <label for="nama">Maskapai:</label>
-        <input type="text" class="form-control" id="maskapai" name="maskapai" required>
-      </div>
-      <div class="form-group ">
-      <label for="inputState">Bandara Asal:</label>
-      <select id="inputState" class="form-control">
-        <option selected>Choose...</option>
-        <option>...</option>
-      </select>
+    <div class="container">
+        <h1 class="mt-5">Pendaftaran Rute Penerbangan</h1>
+        
+        <!-- Form berfungsi untuk mengirim data dari input -->
+        <form method="POST" action="form.php">
+
+            <!-- Input Maskapai -->
+            <div class="form-group">
+                <label for="nama">Maskapai:</label>
+                <input type="text" class="form-control" id="maskapai" name="maskapai" required>
+            </div>
+
+            <!-- Input Bandara Asal -->
+            <div class="form-group ">
+                <label for="inputState">Bandara Asal:</label>
+                <select id="inputState" class="form-control" name="bandaraAsal">
+                    <option selected disabled>Choose...</option>
+                    
+                    <?php
+                    // File Json yang akan dibaca
+                    $file = 'bandaraAsal.json';
+
+                    // Mengambil file Json
+                    $getFile = file_get_contents($file);
+
+                    // Mendecode Json atau mengubah json menjadi Array
+                    $datas = json_decode($getFile, true);
+                    
+                    // Membaca data dari Array menggunakan perulangan foreach
+                    foreach ($datas as $data) {
+                      // Melakukan pengecekan jika data dari 'bandaraAsal' ada, maka akan ditampilkan nama 'bandaraAsal' tersebut
+                        if (isset($data['bandaraAsal'])) {
+                            echo '<option>' . $data['bandaraAsal'] . '</option>';
+                        } else {
+                            echo 'Tidak Ada';
+                        }
+                    }
+                    ?>
+
+                </select>
+            </div>
+            <div class="form-group ">
+                <label for="inputState">Bandara Tujuan:</label>
+                <select id="inputState" class="form-control" name="bandaraTujuan">
+                    <option selected>Choose...</option>
+
+                    <?php
+                    // File Json yang akan dibaca
+                    $file = 'bandaraTujuan.json';
+                   
+                    // Mengambil file Json
+                    $getFile = file_get_contents($file);
+                    
+                    // Mendecode Json atau mengubah json menjadi Array
+                    $datas = json_decode($getFile, true);
+                    
+                    // Membaca data dari Array menggunakan perulangan foreach
+                    foreach ($datas as $data) {
+                      // Melakukan pengecekan jika data dari 'bandaraTujuan' ada, maka akan ditampilkan nama 'bandaraTujuan' tersebut
+                      if (isset($data['bandaraTujuan'])) {
+                            echo '<option>' . $data['bandaraTujuan'] . '</option>';
+                        } else {
+                            echo 'Tidak Ada';
+                        }
+                    }
+                    ?>
+
+                </select>
+            </div>
+
+            <!-- Input Harga Tiker -->
+            <div class="form-group">
+                <label for="harga">Harga Tiket:</label>
+                <input type="number" class="form-control" id="harga" name="harga" required>
+            </div>
+
+            <!-- Button untuk mengirim data -->
+            <button type="submit" class="btn btn-primary" name="submit">SUBMIT</button>
+        </form>
     </div>
-    <div class="form-group ">
-      <label for="inputState">Bandara Tujuan:</label>
-      <select id="inputState" class="form-control">
-        <option selected>Choose...</option>
-        <option>...</option>
-      </select>
-    </div>
-      <div class="form-group">
-        <label for="umur">Harga Tiket:</label>
-        <input type="number" class="form-control" id="umur" name="umur" required>
-      </div>
-      <button type="submit" class="btn btn-primary" name="submit">SUBMIT</button>
-    </form>
-  </div>
-</body>
 
 <?php
-//   if (isset($_POST['submit'])) {
-//     $nama = $_POST['nama'];
-//     $nomor = $_POST['nomor'];
-//     $umur = $_POST['umur'];
+// Memeriksa apakah form telah dikirim atau tidak melalui tombol 'Submit'.
+if (isset($_POST['submit'])) {
+    $maskapai = $_POST['maskapai'];
+    $bandaraAsal = $_POST['bandaraAsal'];
+    $bandaraTujuan = $_POST['bandaraTujuan'];
+    $harga = $_POST['harga'];
 
+    // Membaca, mengambil, dan mengubah file Json menjadi Array
+    $bandaraAsalData = json_decode(file_get_contents('bandaraAsal.json'), true);
+    $bandaraTujuanData = json_decode(file_get_contents('bandaraTujuan.json'), true);
 
-    echo '<!DOCTYPE html>';
-    echo '<html lang="id">';
-    echo '<head>';
-    echo '<title>Data Diri</title>';
-    echo '<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">';
-    echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>';
-    echo '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>';
-    echo '</head>';
-    echo '<body>';
-    echo '<div class="container mt-5">';
-    echo '<h1>Data Diri</h1>';
-    echo '<table class="table table-bordered">';
-    echo '<thead class="thead-dark">';
-    echo '<tr>';
-    echo '<th>Nama</th>';
-    echo '<th>No. WhatsApp</th>';
-    echo '<th>Umur</th>';
-    echo '</tr>';
-    echo '</thead>';
-    echo '</table>';
-    echo '</div>';
-    echo '</body>';
-    echo '</html>';
+    // Melakukan inisialisasi sebagai nilai awal
+    $pajakAsal = 0;
+    $pajakTujuan = 0;
+
+    // Membaca data dari Array menggunakan perulangan foreach
+    foreach ($bandaraAsalData as $data) {
+      // Memeriksa apabila data dari 'bandaraAsal' sesuai dengan nilai input yang dikirim maka akan ditambahkan pajak
+        if ($data['bandaraAsal'] === $bandaraAsal) {
+            $pajakAsal = $data['pajak'];
+            // Menghentikan perulangan jika sesuai dengan kondisi
+            break;
+        }
+    }
+
+    // Membaca data dari Array menggunakan perulangan foreach
+    foreach ($bandaraTujuanData as $data) {
+      // Memeriksa apabila data dari 'bandaraTujuan' sesuai dengan nilai input yang dikirim maka akan ditambahkan pajak
+        if ($data['bandaraTujuan'] === $bandaraTujuan) {
+            $pajakTujuan = $data['pajak'];
+            // Menghentikan perulangan jika sesuai dengan kondisi
+            break;
+        }
+    }
+
+    function totalPajak($pajakAsal, $pajakTujuan){
+      return $pajakAsal + $pajakTujuan;
+    } 
+
+    function totalHarga($totalPajak, $harga){
+      return $totalPajak + $harga;
+    }
+
+    $totalPajak = totalPajak($pajakAsal, $pajakTujuan);
+    $totalHarga = totalHarga($totalPajak, $harga);
+
+    // Membaca, mengambil, dan mengubah file Json menjadi Array
+    $file = 'data.json';
+    $jsonfile = file_get_contents($file);
+    $dataJson = json_decode($jsonfile, true);
+
+    // Data-data yang akan disimpan dalam Json
+    $dataJson[] = array(
+        'maskapai' => $maskapai,
+        'bandaraAsal' => $bandaraAsal,
+        'bandaraTujuan' => $bandaraTujuan,
+        'harga' => $harga,
+        'pajakAsal' => $pajakAsal,
+        'pajakTujuan' => $pajakTujuan,
+        'totalPajak' => $totalPajak,
+        'totalHarga' => $totalHarga
+      );
+
+    // Mengencode dari Array menjadi Json
+    $jsonPost = json_encode($dataJson, JSON_PRETTY_PRINT);
+
+    // Menyimpan data ke dalam data Json
+    file_put_contents('data.json', $jsonPost);
+
+    // Mengarahkan kembali ke Form dan menghentikan eksekusi program
+    header('Location: form.php');
+    exit;
+}
 ?>
 
+    <div class="container mt-5">
+        <h1>Data Diri</h1>
+        <table class="table table-bordered">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Nama</th>
+                    <th>Asal Penerbangan</th>
+                    <th>Tujuan Penerbangan</th>
+                    <th>Harga Tiket</th>
+                    <th>Pajak</th>
+                    <th>Total Harga Tiket</th>
+                </tr>
 
+                  <?php
+                    // Membaca, mengambil, dan mengubah file Json menjadi Array
+                    $jsonFile = 'data.json';
+                    $getFile = file_get_contents($jsonFile);
+                    $datas = json_decode($getFile, true);
+                    
+                    // Melakukan pengecekan jika data dari data.json tidak kosong maka eksekusi program akan dilakukan
+                    if(!empty($datas)){
+                      // Mengekstrak kolom 'maskapai' dari data.json
+                      $maskapaiColumn = array_column($datas, 'maskapai');
+
+                      // Mengurutkan data berdasarkan kolom 'maskapai'
+                      array_multisort($maskapaiColumn, SORT_ASC, $datas);
+                    }
+
+                    // Melakukan pengecekan jika data dari data.json tidak kosong maka eksekusi program akan dilakukan
+                    if(!empty($datas)){
+                      // Membaca data dari Array menggunakan perulangan foreach
+                      foreach($datas as $data){
+                        // Menampilkan output tabel
+                        echo '<tr>';
+                        echo '<td>'. $data['maskapai'] . "</td>";
+                        echo '<td>'. $data['bandaraAsal'] . "</td>";
+                        echo '<td>'. $data['bandaraTujuan'] . "</td>";
+                        echo '<td>'. $data['harga'] . "</td>";
+                        echo '<td>'. $data['totalPajak']  . "</td>";
+                        echo '<td>'. $data['totalHarga'] . "</td>";
+                        echo '</tr>';
+                      }
+                  } else{
+                    // Jika tidak ada data pada data.json maka akan menampilkan 'Tidak ada data'
+                    echo '<tr><td colspan="6" class="text-center" style="font-weight:bold;">Tidak ada data</td></tr>';
+                  }
+                ?>
+            </thead>
+        </table>
+    </div>
+
+</body>
 </html>
